a9230e3110d9e9d966fd7640cc5e2bcdb54b8673
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2022-07-30 19:29:34 -0500

    class foo bar
    Creación de clase foo bar

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // class A
+let classA = class {
+    constructor() {}
+    foo() {
+        return "foo";
+    }
+    bar() {
+        return "bar";
+    }
+}
+
+let objA = new classA();
+console.log(objA.foo(), objA.bar());
 
 // Class B
 
